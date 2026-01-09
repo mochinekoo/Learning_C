ae9ec9bf65f131d84fa158bb8a7310892d211676
mochineko <mochinekoo0113@gmail.com>
2026-01-10 00:55:43 +0900

    [add]　構造体を追加しました

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+struct Location {
+	int x;
+	int y;
+};
+
 int main() {
-	printf("Hello, World!");
+	printf("Hello, World!\n");
+
+	struct Location loc;
+	loc.x = 10;
+	loc.y = 20;
+	printf("Location: (%d, %d)\n", loc.x, loc.y);
 	return 0;
 }
